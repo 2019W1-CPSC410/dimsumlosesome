e629859141917683975be221d28a2e6fc3f6ce24
Patrick Hopkins <patrickdanielhopkins@gmail.com>
2019-11-20 20:29:49 -0800

    created initial client to interface with github and sample script

--- src/GitHubAPI/GitHubClient.js
+++ src/GitHubAPI/GitHubClient.js
@@ -1,7 +1,83 @@
+import axios from "axios";
+
+const baseUrl = "https://api.github.com/repos/";
+
+let headers = {
+    headers: {
+        'User-Agent': 'request'
+    }
+};
 
 class GitHubClient {
 
-    constructor() {}
+    // takes an owner and a repo, see github url to repo for details
+    constructor(owner, repo) {
+        this.owner = owner;
+        this.repo = repo;
+    }
+
+    getBaseUrl() {
+        return baseUrl + `${this.owner}/${this.repo}/`;
+    }
+
+    // Gets all pull request for given repo
+    async getPRForRepo(state="all") {
+        try {
+            const response = await axios.get(this.getBaseUrl() + `pulls?state=${state}`, headers);
+            let data = response.data;
+            data = data.map((pr) => {
+                return {"created_at": pr.created_at,
+                    "updated_at": pr.updated_at,
+                    "closed_at": pr.closed_at,
+                    "merged_at": pr.merged_at,
+                    "merge_commit_sha": pr.merge_commit_sha,
+                    "number": pr.number}
+            });
+            console.log(data);
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // gets the files for a given pull request
+    async getFilesForPr(pull_number) {
+        if (!pull_number) {
+            return;
+        }
+        try {
+            const response = await axios.get(this.getBaseUrl() + `pulls/${pull_number}/files`, headers);
+            let data = response.data;
+            data = data.map((file) => {
+                return {
+                    "raw_url": file.raw_url,
+                    "additions": file.additions,
+                    "deletions": file.deletions,
+                    "changes": file.changes
+                    }
+            });
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // for a given file gets a string representation of that file using the 'raw url'
+    // could potentially save save this file in its correct format (e.g. javascript)
+    // and run linter on files 1 by 1
+    async getFileString(raw_url) {
+        try {
+            const response = await axios.get(raw_url, headers);
+            let data = response.data;
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getCommitsForPR(prId) {}
+
+    async getFileForCommit(commitId) {}
 
 }
 
--- src/MainApi.js
+++ src/MainApi.js
@@ -1,5 +1,50 @@
 import GitHubClient from "./GitHubAPI/GitHubClient";
 
-let client = new GitHubClient();
+const owner = "uber";
+const repo = "react-map-gl";
 
-console.log('here');
+// create client for repo and owner
+let client = new GitHubClient(owner, repo);
+
+let prs = [];
+
+// get all pull request for a given repo
+let response = client.getPRForRepo();
+
+// when request for prs in a repo returns, get the files for each pull request
+// based on the pull request number
+response.then((result) => {
+    result.map((pr) => {
+        console.log('pushing to prs');
+        prs.push(client.getFilesForPr(pr.number))
+    })}).catch((error) => {
+    console.log('failed here!!');
+});
+
+let files = [];
+// using raw_urls of files in pull requests
+// get the string representation of the files
+Promise.all(prs).then((values) => {
+    console.log('first promise.all');
+    values.map((file) => {
+        files.push(client.getFileString(file.raw_url));
+        }
+    )
+});
+
+// print the file strings -> could possibly run routine that:
+//    1. checks if file is javascript file
+//    2. if so saves it as javascript file in given directory
+//    3. runs linter on file and counts bug
+//    4. repeat for all files!!
+//    5. pipeline data for analysis (could be a lot of data, want to focus on small repos)
+Promise.all(files).then((fileStrings) => {
+    console.log('second promise.all');
+
+    fileStrings.map((fileString) => { console.log(fileString)})
+});
+
+
+
+
+
