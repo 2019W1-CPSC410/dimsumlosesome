e1dac9c2bbb2a7dd871c2296024acbeb31393b35
Candice <candice@aytech.ca>
2019-11-22 00:12:02 -0800

    Changed App from js to jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TextField } from '@material-ui/core';
 
-function App() {
+const App = (props) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.jsx</code> and save to reload.
         </p>
+        <TextField variant="outlined" />
         <a
           className="App-link"
           href="https://reactjs.org"
