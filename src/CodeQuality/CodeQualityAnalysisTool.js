e975de8496798a0c3fac2a8eabd0eb2e2ea2ecbb
Candice <candice@aytech.ca>
2019-11-22 20:52:49 -0800

    Used require to import SourceCode, Linter, and Parser in API

--- src/CodeQuality/CodeQualityAnalysisTool.js
+++ src/CodeQuality/CodeQualityAnalysisTool.js
@@ -1,5 +1,46 @@
 import axios from 'axios';
 
+const SourceCode = require('eslint').SourceCode;
+const Linter = require('eslint').Linter;
+const parseForESLint = require('babel-eslint').parseForESLint;
+
+// Linter configurations
+const linter = new Linter();
+linter.defineParser('babel-eslint', { parseForESLint });
+const verifyConfig = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    allowImportExportEverywhere: false,
+    codeFrame: true,
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  useEslintrc: false,
+  rules: {
+    semi: 2,
+    quotes: ["error", "single"],
+  },
+};
+
 class CodeQualityAnalysisTool {
   constructor() {
 
@@ -13,6 +54,23 @@ class CodeQualityAnalysisTool {
     try {
       const response = await axios.get(url);
       return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getBugsFromFile(url) {
+    try {
+      const file = await this.getFile(url);
+      const lines = SourceCode.splitLines(file);
+      const messages = lines.map((line, index) => {
+        const lineNumber = index + 1;
+        const errors = linter.verify(line, verifyConfig);
+        return ({
+          lineNumber,
+          errors,
+        });
+      });
     } catch (error) {
       console.log(error);
     }
