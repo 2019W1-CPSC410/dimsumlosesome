12bc6e46ec54439e7daf059447fca821f2963a75
rubenprograms <gonsalespalazio@gmail.com>
2019-11-24 04:48:45 -0800

    Refactored code to correspond with the interface defined to the analysis component.

--- src/Visualization/Visualization.js
+++ src/Visualization/Visualization.js
@@ -1,39 +1,64 @@
 import React from 'react';
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, MarkSeries, LineSeries } from 'react-vis';
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, MarkSeries } from 'react-vis';
 
 class Visualization extends React.Component {
    constructor(props) {
       super(props);
-      this.state = { repositoryData: props.data.Repository, pullRequestsData: props.data.PullRequests };
+      this.state = { repoCreation: props.data.dateRepoCreated, plannedPRs: props.data.plannedPRs, fastPRs: props.data.fastPRs };
    };
 
-   findLatestTimestampNormalized() {
-      const { pullRequestsData, repositoryData } = this.state;
-      let repoCreationTimestampt = new Date(repositoryData.created_at).getTime();
-      let latestTimestamp = repoCreationTimestampt;
-      for (var i = 0; i < pullRequestsData.length; i++) {
-         let currentTimestamp = new Date(pullRequestsData[i].created_at).getTime();
-         if (currentTimestamp > latestTimestamp) {
-            latestTimestamp = currentTimestamp;
+   processDataForGraph(data) {
+      const NUM_OF_MILLISECONDS_IN_A_DAY = 86400000;
+      /**
+       * Process all PRs data by normalizing the creation date values to display on the graph as well as
+       * finding the largest values for each axis.
+       */
+      let processedPRData = { plannedPRs: [], fastPRs: [], largestDayOffsetFromRepoCreation: 0, highestBugCount: 0 };
+      let repoCreationMilliseconds = (new Date(data.dateRepoCreated)).getTime();
+      // Process the planned PRs:
+      for (let i = 0; i < data.plannedPRs.length; i++) {
+         let currentPRDayOffsetFromRepoCreation = ((new Date(data.plannedPRs[i].datePRCreated)).getTime() - repoCreationMilliseconds) / NUM_OF_MILLISECONDS_IN_A_DAY;
+         let currentBugCount = data.plannedPRs[i].numberOfBugs;
+         if (currentPRDayOffsetFromRepoCreation > processedPRData.largestDayOffsetFromRepoCreation) {
+            processedPRData.largestDayOffsetFromRepoCreation = currentPRDayOffsetFromRepoCreation;
          }
+         if (currentBugCount > processedPRData.highestBugCount) {
+            processedPRData.highestBugCount = currentBugCount;
+         }
+         processedPRData.plannedPRs[i] = { x: currentPRDayOffsetFromRepoCreation, y: currentBugCount };
       }
-      return latestTimestamp - repoCreationTimestampt;
+      // Process the fast PRs:
+      for (let i = 0; i < data.fastPRs.length; i++) {
+         let currentPRDayOffsetFromRepoCreation = ((new Date(data.fastPRs[i].datePRCreated)).getTime() - repoCreationMilliseconds) / NUM_OF_MILLISECONDS_IN_A_DAY;
+         let currentBugCount = data.fastPRs[i].numberOfBugs;
+         if (currentPRDayOffsetFromRepoCreation > processedPRData.largestDayOffsetFromRepoCreation) {
+            processedPRData.largestDayOffsetFromRepoCreation = currentPRDayOffsetFromRepoCreation;
+         }
+         if (currentBugCount > processedPRData.highestBugCount) {
+            processedPRData.highestBugCount = currentBugCount;
+         }
+         processedPRData.fastPRs[i] = { x: currentPRDayOffsetFromRepoCreation, y: currentBugCount };
+      }
+      return processedPRData;
    }
 
    render() {
-      this.findLatestTimestampNormalized();
-      // TODO RUBEN CONTINUE HERE - CONTINUE IMPLEMENTING DATA VISUALIZATION OF MOCK DATA
+      let processedPRData = this.processDataForGraph(this.props.data);
       return (
          <XYPlot width={400} height={400}
-            xDomain={[0, 20]} yDomain={[0, 20]}><XAxis /><YAxis />
+            xDomain={[0, processedPRData.largestDayOffsetFromRepoCreation]} yDomain={[0, processedPRData.highestBugCount]}>
+            <XAxis />
+            <YAxis />
             <HorizontalGridLines />
             <VerticalGridLines />
-            <MarkSeries data={new Array(200).fill(0).reduce((prev, curr) => [...prev, {
-               x: Math.random() * 20,
-               y: Math.random() * 20
-            }], [])} stroke="white"
+            <MarkSeries data={processedPRData.plannedPRs}
+               stroke="black"
                opacityType="category"
-               opacity="0.2" />
+               opacity="1" />
+            <MarkSeries data={processedPRData.fastPRs}
+               stroke="red"
+               opacityType="category"
+               opacity="1" />
          </XYPlot>
       );
    }
